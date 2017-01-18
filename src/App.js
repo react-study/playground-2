0804425ddd3c1d6e7185978debde079233f248e8
yuri <glassyiweb@gmail.com>
2017-01-18 18:57:14 +0900

    todo 추가/삭제

---
+++ src/App.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+
+import Header from './Header';
+import TodoList from './TodoList';
+import Footer from './Footer';
+
+class App extends React.Component {
+    constructor(){
+        super();
+        this.state = {
+            todos : [
+                {id: 1, text: '치킨?'},
+                {id: 2, text: '커피?'}
+            ]
+        }
+    }
+    addTodo(text){
+        this.setState({
+            todos: [
+                ...this.state.todos, {
+                    id: Date.now(),
+                    text
+                }
+            ]
+        });
+    }
+    deleteTodo(todo){
+        const newTodos = [...this.state.todos];
+        const deleteIndex = newTodos.findIndex(v => v.id === todo.id);
+        newTodos.splice(deleteIndex, 1);
+        this.setState({ todos: newTodos });
+    }
+    render(){
+        return (
+            <div className="todo-app">
+                <Header addTodo={text => this.addTodo(text)} />
+                <TodoList
+                    todos={ this.state.todos }
+                    deleteTodo={todo => this.deleteTodo(todo)}
+                />
+                <Footer />
+            </div>
+        )
+    }
+}
+
+export default App;
