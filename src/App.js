e0b52b1228240d908a2828ea2960b113fb6667b5
SEEN <bseeen@gmail.com>
2017-01-19 16:12:34 +0900

    edit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+
 import Header from './Header';
 import TodoList from './TodoList';
 import Footer from './Footer';
@@ -12,7 +13,8 @@ class App extends Component {
                 {id:2,text: 'todo2'},
                 {id:3,text: 'todo3'},
                 {id:4,text: 'todo4'}
-            ]
+            ],
+            editingId: null
         }
     }
     addTodo(text){
@@ -25,19 +27,46 @@ class App extends Component {
             ]
         })
     }
-    deleteTodo(todo){
+    deleteTodo(id){
         const newTodos = [...this.state.todos];
-        const deleteIndex = newTodos.findIndex(v=>v.id===todo.id);
+        const deleteIndex = newTodos.findIndex(v=>v.id=== id);
         newTodos.splice(deleteIndex, 1);
         this.setState({todos : newTodos });
     }
+    editTodo(id){
+        this.setState({
+            editingId: id
+        });
+    }
+    saveTodo(id, newText) {
+        const newTodos = [...this.state.todos];
+        const editIndex = newTodos.findIndex(v => v.id === id);
+        newTodos[editIndex].text = newText;
+        this.setState({
+            todos : newTodos,
+            editingId : null
+        })
+    }
+    cancelEdit(){
+        this.setState({
+            editingId : null
+        })
+    }
     render(){
+        const {
+            todos,
+            editingId
+        } = this.state;
         return(
             <div className="todo-app">
                 <Header addTodo={text => this.addTodo(text)}/>
                 <TodoList
-                    todos={this.state.todos}
+                    todos={todos}
                     deleteTodo={todo => this.deleteTodo(todo)}
+                    saveTodo = {(id,text)=> this.saveTodo(id,text)}
+                    editTodo = {id=>this.editTodo(id)}
+                    editingId = {editingId}
+                    cancelEdit = {()=>this.cancelEdit()}
                     />
                 <Footer />
             </div>
