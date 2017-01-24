32ecc1ac7261150a0b310a58da7538655bb7f94d
Yang Hee kyung <yangheekyung@Yangui-MacBook-Pro.local>
2017-01-24 21:30:33 +0900

    router tet

---
+++ src/routerTest/Container.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import {Router, Route, IndexRoute, Redirect, browserHistory} from 'react-router';
+
+import App from './App';
+import {Home, About, Name, Portfolio} from './Components';
+
+const Container = () => (
+    <Router history={browserHistory}>
+        <Route path="/" component={App}>
+            <IndexRoute component={Home}/>
+            <Route path="about" component={About}>
+                <Route path="name" component={Name}/>
+                <Route path="redirect0" onEnter={(nextState, replace) => replace('/portfolio/0')}/>
+                <Redirect from="redirect1" to="/portfolio/1"/>
+            </Route>
+            <Route path="portfolio(/:id)" component={Portfolio}/>
+        </Route>
+    </Router>
+);
+
+export default Container;
