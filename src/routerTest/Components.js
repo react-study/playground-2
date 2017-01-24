3e0c6b8bff1f7358f825dae1e826ffbddbac9cdd
jzee <samso9@naver.com>
2017-01-24 21:30:50 +0900

    router test

---
+++ src/routerTest/Components.js
@@ -0,0 +1,32 @@
+import React from 'react';
+
+export const Home = () => (<h2>HOME</h2>);
+export const About = ({children}) => (
+    <div>
+            <h2>About</h2>
+            {children}
+    </div>
+);
+
+export const Name = () => (<h3>gom</h3>);
+
+const portfolioList = [
+    {id: 0, text: 'portfolio #0'},
+    {id: 1, text: 'portfolio #1'},
+    {id: 2, text: 'portfolio #2'},
+];
+
+export const Portfolio = ({
+    routeParams: {id}
+}) => {
+    const filteredList = id ? portfolioList.filter(p => p.id == id) : portfolioList;
+    const renderList = filteredList.map(v => (
+        <li key={v.id}>{v.text}</li>
+    ));
+    return (
+        <div>
+            <h2>Portfolio</h2>
+            <ul>{renderList}</ul>
+        </div>
+    );
+};
