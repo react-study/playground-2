f469cba6db41550e641af0bae677d56128b92ece
SEEN <bseeen@gmail.com>
2017-02-02 18:10:47 +0900

    ajax

--- src/App.js
+++ src/App.js
@@ -1,52 +1,83 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 import Header from './Header';
 import TodoList from './TodoList';
 import Footer from './Footer';
 
+const axiosApi = axios.create({
+    baseURL: 'http://localhost:2403/todos',
+    timeout: 1000,
+});
+
+const ax = ({
+    method = 'put',
+    url = '/',
+    data = null,
+    res,
+    err = err => { console.error(err); }
+    })=> axiosApi[method](url, data).then(res).catch(err);
+
 class App extends Component {
-    constructor(){
+    constructor() {
         super();
-        this.state={
-            todos : [
-                {id:1,text: 'todo1'},
-                {id:2,text: 'todo2'},
-                {id:3,text: 'todo3'},
-                {id:4,text: 'todo4'}
-            ],
-            editingId: null,
-            filterName : 'all'
+        this.state = {
+            todos: [],
+            editingId: null
         };
     }
-
+    componentWillMount() {
+        ax({
+            method: 'get',
+            res: ({ data }) => {
+                this.setState({
+                    todos: data
+                });
+            }
+        });
+    }
     addTodo(text) {
-        this.setState({
-            todos: [
-                ...this.state.todos, {
-                    id: Date.now(),
-                    text
-                }
-            ]
+        ax({
+            method: 'post',
+            data: { text },
+            res: ({data}) => {
+                this.setState({
+                    todos: [...this.state.todos, data]
+                });
+            }
         });
     }
     deleteTodo(id) {
-        const newTodos = [...this.state.todos];
-        const deleteIndex = newTodos.findIndex(v => v.id === id);
-        newTodos.splice(deleteIndex, 1);
-        this.setState({ todos: newTodos });
+        ax({
+            method: 'delete',
+            url: `/${id}`,
+            res: () => {
+                const newTodos = [...this.state.todos];
+                const deleteIndex = newTodos.findIndex(v => v.id === id);
+                newTodos.splice(deleteIndex, 1);
+                this.setState({ todos: newTodos });
+            }
+        });
     }
     editTodo(id) {
         this.setState({
             editingId: id
         });
     }
+
     saveTodo(id, newText) {
-        const newTodos = [...this.state.todos];
-        const editIndex = newTodos.findIndex(v => v.id === id);
-        newTodos[editIndex].text = newText;
-        this.setState({
-            todos: newTodos,
-            editingId: null
+        ax({
+            url: `/${id}`,
+            data: { text: newText },
+            res: ({data}) => {
+                const newTodos = [...this.state.todos];
+                const editIndex = newTodos.findIndex(v => v.id === id);
+                newTodos.splice(editIndex, 1, data);
+                this.setState({
+                    todos: newTodos,
+                    editingId: null
+                });
+            }
         });
     }
     cancelEdit() {
@@ -54,41 +85,63 @@ class App extends Component {
             editingId: null
         });
     }
+
     toggleTodo(id) {
-        const newTodos = [...this.state.todos];
-        const toggleIndex = newTodos.findIndex(v => v.id === id);
-        newTodos[toggleIndex].isDone = !newTodos[toggleIndex].isDone;
-        this.setState({
-            todos: newTodos
+        const isDone = this.state.todos.find(v => v.id === id).isDone;
+        ax({
+            url: `/${id}`,
+            data: { isDone: !isDone },
+            res: ({data}) => {
+                const newTodos = [...this.state.todos];
+                const editIndex = newTodos.findIndex(v => v.id === id);
+                newTodos.splice(editIndex, 1, data);
+                this.setState({
+                    todos: newTodos
+                });
+            }
         });
     }
     toggleAll() {
         const isAll = this.state.todos.every(v => v.isDone);
-        const newTodos = this.state.todos.map(todo => {
-            todo.isDone = !isAll;
-            return todo;
-        });
-        this.setState({
-            todos: newTodos
+        const axp = this.state.todos
+            .map(todo => ax({
+                method: 'put',
+                url: `/${todo.id}`,
+                data: { isDone: !isAll }
+            }));
+        axios.all(axp).then(res => {
+            this.setState({
+                todos: res.map(res => res.data)
+            });
         });
     }
-    selectFilter(filter) {
-        this.setState({ filterName : filter });
-    }
+
     deleteCompleted() {
+        const axp = this.state.todos
+            .filter(v => v.isDone)
+            .map(todo => ax({
+                method: 'delete',
+                url: `/${todo.id}`
+            }));
         const newTodos = this.state.todos.filter(v => !v.isDone);
-        this.setState({ todos: newTodos });
+        axios.all(axp)
+            .then(()=> {
+                this.setState({ todos: newTodos });
+            })
+            .catch(err => { console.err(err); });
     }
     render(){
+        console.log('render');
         const {
             todos,
-            editingId,
-            filterName
+            editingId
             } = this.state;
+        const filterName = this.props.routeParams.filter;
+
         const activeLength = todos.filter(v => !v.isDone).length;
         const viewTodos = todos.filter(({isDone}) => {
             if(
-                filterName === 'all' ||
+                !filterName ||
                 (filterName === 'active' && !isDone) ||
                 (filterName === 'completed' && isDone)
             ) return true;
@@ -112,7 +165,6 @@ class App extends Component {
                     filterName      = {filterName}
                     activeLength    = {activeLength}
                     isSomeCompleted = {isSomeCompleted}
-                    selectFilter    = {filter => this.selectFilter(filter)}
                     deleteCompleted = {()=> this.deleteCompleted()}
                     />
             </div>
