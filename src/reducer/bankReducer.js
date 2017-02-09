a473ab1ad45a027093baeb0b9dae9c9c49704a98
SEEN <bseeen@gmail.com>
2017-02-09 20:05:50 +0900

    redux-example

---
+++ src/reducer/bankReducer.js
@@ -0,0 +1,39 @@
+const initialState = {
+    account : [],
+    total : 0
+};
+const bankReducer = (state = initialState, action) => {
+    switch(action.type){
+    case 'SAVE_MONEY': {
+        const newResult = state.total + +action.val;
+        const newAccount = [
+            ...state.account, {
+                type: 'save',
+                val: action.val,
+                result: newResult
+            }
+        ];
+        return {
+            account: newAccount,
+            total: newResult
+        };
+    }
+    case 'WITHDRAW_MONEY': {
+        const newResult = state.total - +action.val;
+        const newAccount = [
+            ...state.account, {
+                type: 'withdraw',
+                val:action.val,
+                result: newResult
+            }
+        ];
+        return {
+            account : newAccount,
+            total : newResult
+        };
+    }
+    default :
+        return state;
+    }
+};
+export default bankReducer;
---
+++ src/store.js
@@ -0,0 +1,10 @@
+import { createStore } from 'redux';
+import bankReducer from './reducer/bankReducer';
+
+const store = createStore(
+    bankReducer,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
+
+export default store;
