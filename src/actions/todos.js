d57c9038f1ede1d5c32a1781906d60f1c8e10c3c
gomugom <power4ce@gmail.com>
2017-02-14 22:03:48 +0900

    리덕스 로컬 전환

---
+++ src/actions/todos.js
@@ -0,0 +1,52 @@
+// actions/todos.js
+
+const TodosActions = {
+    addTodo(text) {
+        return {
+            type: 'ADD_TODO',
+            text
+        };
+    },
+    deleteTodo(id) {
+        return {
+            type: 'DELETE_TODO',
+            id
+        };
+    },
+    editTodo(id) {
+        return {
+            type: 'EDIT_TODO',
+            id
+        };
+    },
+    saveTodo(id, newText) {
+        return {
+            type: 'SAVE_TODO',
+            id,
+            newText
+        };
+    },
+    cancelEdit() {
+        return {
+            type: 'CANCEL_EDIT'
+        };
+    },
+    toggleTodo(id) {
+        return {
+            type: 'TOGGLE_TODO',
+            id
+        };
+    },
+    toggleAll() {
+        return {
+            type: 'TOGGLE_ALL'
+        };
+    },
+    deleteCompleted() {
+        return {
+            type: 'DELETE_COMPLETED'
+        };
+    }
+};
+
+export default TodosActions;
---
+++ src/reducers/todos.js
@@ -0,0 +1,84 @@
+// reducers/todos.js
+
+const initialState = {
+    todos: [
+        {id: 1, text: '치킨에 맥주 한 잔'},
+        {id: 2, text: '삼겹살에 소주 한 잔'},
+        {id: 3, text: '리코타샐러드에 봉골레 파스타'},
+        {id: 4, text: '떡순튀'}
+    ],
+    editingId: null
+};
+
+const todoReducer = (state = initialState, action) => {
+    switch(action.type) {
+    case 'ADD_TODO':
+        return {
+            todos: [
+                ...state.todos, {
+                    id: Date.now(),
+                    text: action.text
+                }
+            ],
+            editingId: null
+        };
+    case 'DELETE_TODO': {
+        const newTodos = [...state.todos];
+        const deleteIndex = newTodos.findIndex(v => v.id === action.id);
+        newTodos.splice(deleteIndex, 1);
+        return {
+            todos: newTodos,
+            editingId: null
+        };
+    }
+    case 'EDIT_TODO':
+        return {
+            todos: state.todos,
+            editingId: action.id
+        };
+    case 'SAVE_TODO': {
+        const newTodos = [...state.todos];
+        const editIndex = newTodos.findIndex(v => v.id === action.id);
+        newTodos[editIndex].text = action.newText;
+        return {
+            todos: newTodos,
+            editingId: null
+        };
+    }
+    case 'CANCEL_EDIT':
+        return {
+            todos: state.todos,
+            editingId: null
+        };
+    case 'TOGGLE_TODO': {
+        const newTodos = [...state.todos];
+        const toggleIndex = newTodos.findIndex(v => v.id === action.id);
+        newTodos[toggleIndex].isDone = !newTodos[toggleIndex].isDone;
+        return {
+            todos: newTodos,
+            editingId: null
+        };
+    }
+    case 'TOGGLE_ALL': {
+        const isAll = state.todos.every(v => v.isDone);
+        const newTodos = state.todos.map(todo => {
+            todo.isDone = !isAll;
+            return todo;
+        });
+        return {
+            todos: newTodos,
+            editingId: null
+        };
+    }
+    case 'DELETE_COMPLETED': {
+        const newTodos = state.todos.filter(v => !v.isDone);
+        return {
+            todos: newTodos,
+            editingId: null
+        };
+    }
+    default: return state;
+    }
+};
+
+export default todoReducer;
